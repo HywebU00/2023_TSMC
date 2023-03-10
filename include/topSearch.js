585295db61702b904f5dedcacc1d1bcc55293c00
Tseng <tseng7510@gmail.com>
2023-03-10 17:26:15 +0800

    更新default搜尋/新增頁面

--- include/topSearch.js
+++ include/topSearch.js
@@ -7,12 +7,9 @@
                     <input type="text" id="" placeholder="請輸入文字" title="請輸入文字" />
 
                     <button class="kitBtn kitBtn_radius kitBtn_s kitBtn_icon" type="button">
-                      <i class="kitIcon"><img src="images/icon/icon_close.svg" alt="" /></i>
+                      <i class="kitIcon"><img src="images/icon/icon_search.svg" alt="" /></i>
                     </button>
                   </div>
-                  <button class="kitBtn kitBtn_icon" type="button">
-                    <i class="kitIcon"><img src="images/icon/icon_search.svg" alt="" /></i>
-                  </button>
                 </div>
             </div>
           </div>
